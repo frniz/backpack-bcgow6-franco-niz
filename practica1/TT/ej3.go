3e5f1d31f45810a9806df112bbb99f15dcddf03e
frniz <franco.niz@mercadolibre.com>
2022-09-24 10:17:25 -0300

    Ej3 con modificaciones

--- practica1/TT/ej3.go
+++ practica1/TT/ej3.go
@@ -4,10 +4,18 @@ import "fmt"
 
 func main() {
 
-	var meses = map[int]string{1: "Enero", 2: "Febrero", 3: "Marzo"}
+	var meses = map[int]string{1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio",
+		7: "Julio", 8: "Agosto", 9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"}
 
-	numero := 1
+	fmt.Println("Ingrese el numero de mes del cual quiera saber el nombre: ")
 
-	fmt.Println(meses[numero])
+	var numeroDeMes int
+	fmt.Scanln(&numeroDeMes)
+
+	if numeroDeMes > 0 && numeroDeMes < 13 {
+		fmt.Println("\n\n", meses[numeroDeMes])
+	} else {
+		fmt.Println("\n\nNo corresponde al numero de un mes")
+	}
 
 }
