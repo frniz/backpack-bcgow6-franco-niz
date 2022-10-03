675e229c9ec44846ce0c2852d961c3e9e99a2d61
frniz <franco.niz@mercadolibre.com>
2022-10-03 16:30:19 -0300

    rm go.work

--- go-web/practica1/main.go
+++ go-web/practica1/main.go
@@ -10,6 +10,11 @@ package main
 // Genera un handler para el endpoint llamado “GetAll”.
 // Crea una slice de la estructura, luego devuelvela a través de nuestro endpoint.
 
+// Según la temática elegida, necesitamos agregarles filtros a nuestro endpoint, el mismo se tiene que poder filtrar por todos los campos.
+// Dentro del handler del endpoint, recibí del contexto los valores a filtrar.
+// Luego genera la lógica de filtrado de nuestro array.
+// Devolver por el endpoint el array filtrado.
+
 import (
 	"encoding/json"
 	"os"
