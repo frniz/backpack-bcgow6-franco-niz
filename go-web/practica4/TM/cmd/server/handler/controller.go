d3ec725fba58a471ef8b48ba974ad0f3ba42d628
frniz <franco.niz@mercadolibre.com>
2022-10-06 17:10:42 -0300

    Agregado response

---
+++ go-web/practica4/TM/cmd/server/handler/controller.go
@@ -0,0 +1,255 @@
+package handler
+
+// Se debe separar la estructura del proyecto, como segundo paso se debe generar el paquete server donde se agregaran las
+// funcionalidades del proyecto que dependan de paquetes externos y el main del programa.
+
+// Dentro del paquete deben estar:
+// El main del programa.
+// - Se debe importar e inyectar el repositorio, servicio y handler
+// - Se debe implementar el router para los diferentes endpoints
+// El paquete handler con el controlador de la entidad seleccionada.
+// - Se debe generar la estructura request
+// - Se debe generar la estructura del controlador que tenga como campo el servicio
+// - Se debe generar la función que retorne el controlador
+// - Se deben generar todos los métodos correspondientes a los endpoints
+
+import (
+	transactions "backpack-bcgow6-franco-niz/go-web/practica4/TM/internal/transactions"
+	"backpack-bcgow6-franco-niz/go-web/practica4/TM/pkg/web"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type request struct {
+	ID       int     `json:"id"`
+	Code     string  `json:"code" binding:"required"`
+	Currency string  `json:"currency"`
+	Price    float64 `json:"price" binding:"required"`
+	Emitter  string  `json:"emitter"`
+	Receiver string  `json:"receiver"`
+	Date     string  `json:"date"`
+}
+
+type Transaction struct {
+	service transactions.Service
+}
+
+func NewTransaction(s transactions.Service) *Transaction {
+	return &Transaction{
+		service: s,
+	}
+}
+
+func (c *Transaction) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, web.InvalidToken))
+			return
+
+		}
+
+		t, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+			return
+		}
+
+		if len(t) == 0 {
+			ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, web.NotElements))
+		}
+
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, t, ""))
+	}
+}
+
+func (c *Transaction) Store() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, web.InvalidToken))
+			return
+		}
+
+		var req request
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			var message string
+			if req.Code == "" {
+				message = message + fmt.Sprintf("El campo Code es requerido. ")
+			}
+
+			if req.Price == 0 {
+				message = message + fmt.Sprintf("El campo Price es requerido. ")
+			}
+
+			ctx.JSON(400, web.NewResponse(400, nil, message))
+			return
+		}
+
+		var message string
+		if req.Currency == "" {
+			message = message + fmt.Sprintf("El campo Currency es requerido. ")
+		}
+		if req.Emitter == "" {
+			message = message + fmt.Sprintf("El campo Emitter es requerido. ")
+		}
+		if req.Receiver == "" {
+			message = message + fmt.Sprintf("El campo Receiver es requerido. ")
+		}
+		if req.Date == "" {
+			message = message + fmt.Sprintf("El campo Date es requerido. ")
+		}
+		if message != "" {
+			ctx.JSON(400, web.NewResponse(400, nil, message))
+			return
+		}
+
+		t, err := c.service.Store(req.Code, req.Currency, req.Price, req.Emitter, req.Receiver, req.Date)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, web.NewResponse(http.StatusNotFound, nil, err.Error()))
+			return
+		}
+
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, t, ""))
+	}
+}
+
+func (c *Transaction) Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, web.InvalidToken))
+			return
+		}
+
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": "ID invalido",
+			})
+			return
+		}
+
+		var req request
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			var message string
+			if req.Code == "" {
+				message = message + fmt.Sprintf("El campo Code es requerido. ")
+			}
+
+			if req.Price == 0 {
+				message = message + fmt.Sprintf("El campo Price es requerido. ")
+			}
+
+			ctx.String(400, message)
+			return
+		}
+
+		var message string
+		if req.Currency == "" {
+			message = message + fmt.Sprintf("El campo Currency es requerido. ")
+		}
+		if req.Emitter == "" {
+			message = message + fmt.Sprintf("El campo Emitter es requerido. ")
+		}
+		if req.Receiver == "" {
+			message = message + fmt.Sprintf("El campo Receiver es requerido. ")
+		}
+		if req.Date == "" {
+			message = message + fmt.Sprintf("El campo Date es requerido. ")
+		}
+		if message != "" {
+			ctx.String(400, message)
+			return
+		}
+
+		t, err := c.service.Update(int(id), req.Code, req.Currency, req.Price, req.Emitter, req.Receiver, req.Date)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, err.Error())
+			return
+		}
+
+		ctx.JSON(http.StatusOK, t)
+	}
+}
+
+func (c *Transaction) PartialUpdate() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, web.InvalidToken))
+			return
+		}
+
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": "ID invalido",
+			})
+			return
+		}
+
+		var req request
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			var message string
+			if req.Code == "" {
+				message = message + fmt.Sprintf("El campo Code es requerido. ")
+			}
+			if req.Price == 0 {
+				message = message + fmt.Sprintf("El campo Price es requerido. ")
+			}
+			ctx.JSON(400, gin.H{
+				"error": message,
+			})
+			return
+		}
+
+		t, err := c.service.PartialUpdate(int(id), req.Code, req.Price)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, t)
+
+	}
+}
+
+func (c *Transaction) Delete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, web.NewResponse(http.StatusUnauthorized, nil, web.InvalidToken))
+			return
+		}
+
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, "El ID es invalido"))
+			return
+		}
+
+		err = c.service.Delete(int(id))
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, web.NewResponse(http.StatusBadRequest, nil, err.Error()))
+			return
+		}
+
+		ctx.JSON(http.StatusOK, web.NewResponse(http.StatusOK, fmt.Sprintf("La transaccion con id %d fue eliminada", id), ""))
+	}
+}
+
+func tokenValidator(ctx *gin.Context) error {
+	token := ctx.Request.Header.Get("token")
+	if token != os.Getenv("TOKEN") {
+
+		return fmt.Errorf("Error: Token invalido.")
+
+	}
+	return nil
+}
