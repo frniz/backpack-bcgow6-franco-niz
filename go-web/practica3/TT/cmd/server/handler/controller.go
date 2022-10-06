9b55e60ac82473c10d109af64ef5a9490284043a
frniz <franco.niz@mercadolibre.com>
2022-10-06 09:08:24 -0300

    File db

---
+++ go-web/practica3/TT/cmd/server/handler/controller.go
@@ -0,0 +1,267 @@
+package handler
+
+// Se debe separar la estructura del proyecto, como segundo paso se debe generar el paquete server donde se agregaran las
+// funcionalidades del proyecto que dependan de paquetes externos y el main del programa.
+
+// Dentro del paquete deben estar:
+// El main del programa.
+// - Se debe importar e inyectar el repositorio, servicio y handler
+// - Se debe implementar el router para los diferentes endpoints
+// El paquete handler con el controlador de la entidad seleccionada.
+// - Se debe generar la estructura request
+// - Se debe generar la estructura del controlador que tenga como campo el servicio
+// - Se debe generar la función que retorne el controlador
+// - Se deben generar todos los métodos correspondientes a los endpoints
+
+import (
+	transactions "backpack-bcgow6-franco-niz/go-web/practica3/TT/internal/transactions"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type request struct {
+	ID       int     `json:"id"`
+	Code     string  `json:"code" binding:"required"`
+	Currency string  `json:"currency"`
+	Price    float64 `json:"price" binding:"required"`
+	Emitter  string  `json:"emitter"`
+	Receiver string  `json:"receiver"`
+	Date     string  `json:"date"`
+}
+
+type Transaction struct {
+	service transactions.Service
+}
+
+func NewTransaction(s transactions.Service) *Transaction {
+	return &Transaction{
+		service: s,
+	}
+}
+
+func (c *Transaction) GetAll() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+
+		}
+
+		t, err := c.service.GetAll()
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, t)
+	}
+}
+
+func (c *Transaction) Store() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+
+		}
+
+		var req request
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			var message string
+			if req.Code == "" {
+				message = message + fmt.Sprintf("El campo Code es requerido. ")
+			}
+
+			if req.Price == 0 {
+				message = message + fmt.Sprintf("El campo Price es requerido. ")
+			}
+
+			ctx.String(400, message)
+			return
+		}
+
+		var message string
+		if req.Currency == "" {
+			message = message + fmt.Sprintf("El campo Currency es requerido. ")
+		}
+		if req.Emitter == "" {
+			message = message + fmt.Sprintf("El campo Emitter es requerido. ")
+		}
+		if req.Receiver == "" {
+			message = message + fmt.Sprintf("El campo Receiver es requerido. ")
+		}
+		if req.Date == "" {
+			message = message + fmt.Sprintf("El campo Date es requerido. ")
+		}
+		if message != "" {
+			ctx.String(400, message)
+			return
+		}
+
+		t, err := c.service.Store(req.Code, req.Currency, req.Price, req.Emitter, req.Receiver, req.Date)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, err.Error())
+			return
+		}
+
+		ctx.JSON(http.StatusOK, t)
+	}
+}
+
+func (c *Transaction) Update() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+
+		}
+
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": "ID invalido",
+			})
+			return
+		}
+
+		var req request
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			var message string
+			if req.Code == "" {
+				message = message + fmt.Sprintf("El campo Code es requerido. ")
+			}
+
+			if req.Price == 0 {
+				message = message + fmt.Sprintf("El campo Price es requerido. ")
+			}
+
+			ctx.String(400, message)
+			return
+		}
+
+		var message string
+		if req.Currency == "" {
+			message = message + fmt.Sprintf("El campo Currency es requerido. ")
+		}
+		if req.Emitter == "" {
+			message = message + fmt.Sprintf("El campo Emitter es requerido. ")
+		}
+		if req.Receiver == "" {
+			message = message + fmt.Sprintf("El campo Receiver es requerido. ")
+		}
+		if req.Date == "" {
+			message = message + fmt.Sprintf("El campo Date es requerido. ")
+		}
+		if message != "" {
+			ctx.String(400, message)
+			return
+		}
+
+		t, err := c.service.Update(int(id), req.Code, req.Currency, req.Price, req.Emitter, req.Receiver, req.Date)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, err.Error())
+			return
+		}
+
+		ctx.JSON(http.StatusOK, t)
+	}
+}
+
+func (c *Transaction) PartialUpdate() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+
+		}
+
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": "ID invalido",
+			})
+			return
+		}
+
+		var req request
+		if err := ctx.ShouldBindJSON(&req); err != nil {
+			var message string
+			if req.Code == "" {
+				message = message + fmt.Sprintf("El campo Code es requerido. ")
+			}
+			if req.Price == 0 {
+				message = message + fmt.Sprintf("El campo Price es requerido. ")
+			}
+			ctx.JSON(400, gin.H{
+				"error": message,
+			})
+			return
+		}
+
+		t, err := c.service.PartialUpdate(int(id), req.Code, req.Price)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, t)
+
+	}
+}
+
+func (c *Transaction) Delete() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		if err := tokenValidator(ctx); err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+
+		}
+
+		id, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": "ID invalido",
+			})
+			return
+		}
+
+		err = c.service.Delete(int(id))
+		if err != nil {
+			ctx.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"data": fmt.Sprintf("La transaccion con id %d fue eliminada", id),
+		})
+	}
+}
+
+func tokenValidator(ctx *gin.Context) error {
+	token := ctx.Request.Header.Get("token")
+	if token != os.Getenv("TOKEN") {
+
+		return fmt.Errorf("Error: Token invalido.")
+
+	}
+	return nil
+}
---
+++ go-web/practica3/TT/cmd/server/main.go
@@ -0,0 +1,51 @@
+package main
+
+// Ejercicio 1 - Configuración ENV
+
+// Configurar para que el token sea tomado de las variables de entorno al momento de realizar
+// la validación, para eso se deben realizar los siguientes pasos:
+// 1. Configurar la aplicación para que tome los valores que se encuentran en el archivo
+// .env como variable de entorno.
+// 2. Quitar el valor del token del código y agregar como variable de entorno.
+// 3. Acceder al valor del token mediante la variable de entorno.
+
+// Ejercicio 2 - Guardar información
+// Se debe implementar la funcionalidad para guardar la información de la petición en un
+// archivo json, para eso se deben realizar los siguientes pasos:
+// 1. En lugar de guardar los valores de nuestra entidad en memoria, se debe crear un
+// archivo; los valores que se vayan agregando se guardan en él.
+
+// Ejercicio 3 - Leer información
+
+// Se debe implementar la funcionalidad para leer la información requerida en la petición del
+// archivo json generado al momento de guardar, para eso se deben realizar los siguientes
+// pasos:
+
+// 1. En lugar de leer los valores de nuestra entidad en memoria, se debe obtener del
+// archivo generado en el punto anterior.
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+	handler "backpack-bcgow6-franco-niz/go-web/practica3/TT/cmd/server/handler"
+	transactions "backpack-bcgow6-franco-niz/go-web/practica3/TT/internal/transactions"
+	"backpack-bcgow6-franco-niz/go-web/practica3/TT/pkg/store"
+)
+
+func main() {
+	_ = godotenv.Load()
+	db := store.New(store.FileType, "./products.json")
+	repo := transactions.NewRepository(db)
+	service := transactions.NewService(repo)
+	t := handler.NewTransaction(service)
+
+	r := gin.Default()
+	tr := r.Group("/transactions")
+	tr.POST("/", t.Store())
+	tr.GET("/", t.GetAll())
+	tr.PUT("/:id", t.Update())
+	tr.PATCH("/:id", t.PartialUpdate())
+	tr.DELETE("/:id", t.Delete())
+	r.Run()
+}
